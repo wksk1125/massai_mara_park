0b482be7f22dc371267151e2ac69a7463ce6c2bf
zzyzzy <zzyzigy@gmail.com>
2025-01-15 12:05:24 +0900

    initial commit

---
+++ src/test/java/zzyzzy/massai_mara_park/MassaiMaraParkApplicationTests.java
@@ -0,0 +1,13 @@
+package zzyzzy.massai_mara_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassaiMaraParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
